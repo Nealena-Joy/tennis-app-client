4386fb0dc6e65732f71d17a8eb14a63063a13fb5
Nealena Ras <nealenaras@gmail.com>
2021-11-30 16:13:29 -0500

    apiurl

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -2,10 +2,10 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-        APIURL = 'https://tennis-app-njr.herokuapp.com';
+        APIURL = 'http://localhost:3000';
         break;
     case 'tennis-app-client-njr.herokuapp.com':
-        APIURL = 'https://tennis-app-client-njr.herokuapp.com/'
+        APIURL = 'https://tennis-app-njr.herokuapp.com'
 }
 
 export default APIURL;
